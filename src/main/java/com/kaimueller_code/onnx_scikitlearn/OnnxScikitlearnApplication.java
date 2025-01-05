fdae4ae6527301c47eefedd36a77286613f0982e
Kai Mueller <me@kaimueller-code.com>
2025-01-05 19:25:24 +0100

    initial commit

---
+++ src/main/java/com/kaimueller_code/onnx_scikitlearn/OnnxScikitlearnApplication.java
@@ -0,0 +1,13 @@
+package com.kaimueller_code.onnx_scikitlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnnxScikitlearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnnxScikitlearnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kaimueller_code/onnx_scikitlearn/OnnxScikitlearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaimueller_code.onnx_scikitlearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnnxScikitlearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
